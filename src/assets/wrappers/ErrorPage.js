7e3ee25833d8cf36c8414070a53a68b92aab58e2
Асадбек* <asadbekme2002@gmail.com>
2023-01-02 01:55:14 +0500

    Title, Favicon, Normalize.css and Global Styles

---
+++ src/assets/wrappers/ErrorPage.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.main`
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 90vw;
+    max-width: 600px;
+    display: block;
+    margin-bottom: 2rem;
+  }
+  
+  h3 {
+    margin-bottom: 0.5rem;
+  }
+
+  p {
+    margin-top: 0;
+    margin-bottom: 0.5rem;
+    color: var(--grey-500);
+  }
+  
+  a {
+    color: var(--primary-500);
+    text-decoration: underline;
+    text-transform: capitalize;
+  }
+`;
+
+export default Wrapper;
