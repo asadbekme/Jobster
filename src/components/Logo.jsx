fd0676f17b4c116baba1c8145582002a39141581
Асадбек* <asadbekme2002@gmail.com>
2023-01-04 00:49:58 +0500

    Error page

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,9 +2,7 @@
 import logo from '../assets/images/logo.svg';
 
 const Logo = () => {
-  return (
-    <img src={logo} alt="jobster logo" className='logo' />
-  );
+  return <img src={logo} alt="jobster logo" className='logo' />;
 }
 
 export default Logo;
