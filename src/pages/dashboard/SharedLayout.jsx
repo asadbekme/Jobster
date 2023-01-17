4db976fab2d8405f93403ef2367fa7e4a7725e06
Асадбек* <asadbekme2002@gmail.com>
2023-01-17 12:39:22 +0500

    Complete SharedLayout

--- src/pages/dashboard/SharedLayout.jsx
+++ src/pages/dashboard/SharedLayout.jsx
@@ -1,6 +1,21 @@
+import { Outlet } from "react-router-dom";
+import { Navbar, BigSidebar, SmallSidebar } from "../../components";
+import Wrapper from "../../assets/wrappers/SharedLayout"
+
 const SharedLayout = () => {
   return (
-    <div>SharedLayout</div>
+    <Wrapper>
+      <main className="dashboard">
+        <SmallSidebar />
+        <BigSidebar />
+        <div>
+          <Navbar />
+          <div className="dashboard-page">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </Wrapper>
   );
 }
 
