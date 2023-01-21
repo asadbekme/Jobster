60824cdb0579813416c14cdede51f3ddf7e29a9d
Асадбек* <asadbekme2002@gmail.com>
2023-01-21 23:16:57 +0500

    Small Sidebar - Toggle

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -1,9 +1,34 @@
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import { FaTimes } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+import Logo from './Logo';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleSidebar } from '../features/user/userSlice';
 
 const SmallSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  }
+
   return (
     <Wrapper>
-      <h2>SmallSidebar</h2>
+      <div className={isSidebarOpen ? 'sidebar-container show-sidebar' : 'sidebar-container'}>
+        <div className="content">
+          <button 
+            className='close-btn' 
+            onClick={toggle}
+          >
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className='nav-links'>nav links</div>
+        </div>
+      </div>
     </Wrapper>
   );
 }
