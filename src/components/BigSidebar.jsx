c143ea81cef1b736f48a0951bee8902677910136
Асадбек* <asadbekme2002@gmail.com>
2023-01-22 10:06:54 +0500

    BigSidebar Component

--- src/components/BigSidebar.jsx
+++ src/components/BigSidebar.jsx
@@ -1,11 +1,26 @@
+import { useSelector } from "react-redux";
 import Wrapper from "../assets/wrappers/BigSidebar";
+import { Logo, NavLinks } from "./index";
 
 const BigSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+
   return (
     <Wrapper>
-      <h2>BigSidebar</h2>
+      <div
+        className={
+          isSidebarOpen ? "sidebar-container" : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
-}
+};
 
 export default BigSidebar;
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,14 @@ import Logo from "./Logo";
 import FormRow from "./FormRow";
 import Navbar from "./Navbar";
 import SmallSidebar from "./SmallSidebar";
-import BigSidebar from "./BigSidebar"
+import BigSidebar from "./BigSidebar";
+import NavLinks from "./NavLinks";
 
-export { Logo, FormRow, Navbar, SmallSidebar, BigSidebar };
+export {
+    Logo,
+    FormRow,
+    Navbar,
+    SmallSidebar,
+    BigSidebar,
+    NavLinks
+};
