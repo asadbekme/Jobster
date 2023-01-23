216ca03253007c8a2f80a416a0378b9e903b3f33
Асадбек* <asadbekme2002@gmail.com>
2023-01-24 01:01:10 +0500

    Profile Page - Structure

--- README.md
+++ README.md
@@ -1375,27 +1375,29 @@ const Profile = () => {
   const { isLoading, user } = useSelector((store) => store.user);
   const dispatch = useDispatch();
 
-const [userData,setUserData] = useState({
-  name:user?.name ||''
-  email:user?.email ||''
-  lastName:user?.lastName ||''
-  location:user?.location ||''
-})
+  const [userData,setUserData] = useState({
+    name: user?.name || ''
+    email: user?.email || ''
+    lastName: user?.lastName || ''
+    location: user?.location || ''
+  })
 
   const handleSubmit = (e) => {
     e.preventDefault();
-        const { name, email, lastName, location } = userData;
+    const { name, email, lastName, location } = userData;
 
     if (!name || !email || !lastName || !location) {
       toast.error('Please Fill Out All Fields');
       return;
     }
   };
-const handleChange = (e) =>{
-  const name = e.target.name
-  const value = e.target.value
-  setUserData({...userData,[name]:value})
-}
+
+  const handleChange = (e) =>{
+    const name = e.target.name
+    const value = e.target.value
+    setUserData({...userData, [name]: value})
+  }
+
   return (
     <Wrapper>
       <form className='form' onSubmit={handleSubmit}>
--- src/pages/dashboard/Profile.jsx
+++ src/pages/dashboard/Profile.jsx
@@ -1,6 +1,73 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { FormRow } from "../../components";
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+
 const Profile = () => {
+  const { isLoading, user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const [userData, setUserData] = useState({
+    name: user?.name || '',
+    email: user?.email || '',
+    lastName: user?.lastName || '',
+    location: user?.location || ''
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, lastName, location } = userData;
+
+    if (!name || !email || !lastName || !location) {
+      toast.error('Please Fill Out All Fields');
+      return;
+    }
+  }
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUserData({...userData, [name]: value});
+  }
+
   return (
-    <div>Profile</div>
+    <Wrapper>
+      <form className='form' onSubmit={handleSubmit}>
+        <h3>profile</h3>
+
+        <div className='form-center'>
+          <FormRow
+            type='text'
+            name='name'
+            value={userData.name}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type='text'
+            labelText='last name'
+            name='lastName'
+            value={userData.lastName}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type='email'
+            name='email'
+            value={userData.email}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type='text'
+            name='location'
+            value={userData.location}
+            handleChange={handleChange}
+          />
+          <button className='btn btn-block' type='submit' disabled={isLoading}>
+            {isLoading ? 'Please Wait...' : 'save changes'}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
   );
 }
 
